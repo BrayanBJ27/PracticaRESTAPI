ad4f2be0998374182711ab517012314968a7fad2
BrayanBJ27 <xavi_braian@hotmail.com>
2024-06-26 17:13:54 -0500

    Udpate Structure

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,51 +1,33 @@
 // controllers/productController.js
-const Product = require('../models/Product');
+let products = []; // In-memory storage for products
+let id = 1;
 
-exports.getProducts = async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+exports.getProducts = (req, res) => {
+  res.json(products);
 };
 
-exports.getProductById = async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) return res.status(404).json({ message: 'Product not found' });
-    res.json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+exports.getProductById = (req, res) => {
+  const product = products.find(p => p.id === parseInt(req.params.id));
+  if (!product) return res.status(404).json({ message: 'Product not found' });
+  res.json(product);
 };
 
-exports.createProduct = async (req, res) => {
-  const product = new Product(req.body);
-  try {
-    const savedProduct = await product.save();
-    res.status(201).json(savedProduct);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+exports.createProduct = (req, res) => {
+  const product = { id: id++, ...req.body };
+  products.push(product);
+  res.status(201).json(product);
 };
 
-exports.updateProduct = async (req, res) => {
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updatedProduct) return res.status(404).json({ message: 'Product not found' });
-    res.json(updatedProduct);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+exports.updateProduct = (req, res) => {
+  const productIndex = products.findIndex(p => p.id === parseInt(req.params.id));
+  if (productIndex === -1) return res.status(404).json({ message: 'Product not found' });
+  products[productIndex] = { id: products[productIndex].id, ...req.body };
+  res.json(products[productIndex]);
 };
 
-exports.deleteProduct = async (req, res) => {
-  try {
-    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-    if (!deletedProduct) return res.status(404).json({ message: 'Product not found' });
-    res.json({ message: 'Product deleted' });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+exports.deleteProduct = (req, res) => {
+  const productIndex = products.findIndex(p => p.id === parseInt(req.params.id));
+  if (productIndex === -1) return res.status(404).json({ message: 'Product not found' });
+  products.splice(productIndex, 1);
+  res.json({ message: 'Product deleted' });
 };
