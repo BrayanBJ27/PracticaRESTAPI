2543fd26165b933c7395ffc2eb0c53a68de19ab1
BrayanBJ27 <xavi_braian@hotmail.com>
2024-06-26 17:23:36 -0500

    feat: :sparkles: Add Swager

--- app.js
+++ app.js
@@ -2,12 +2,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/products');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use('/api/products', productRoutes);
+
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Minimarket API',
+      version: '1.0.0',
+      description: 'API for managing products, sales, and users in a minimarket',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Error handling middleware
 app.use((err, req, res, next) => {
